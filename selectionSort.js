31209914bfd8621fe8a02aee4ffa1df2984d9e0a
natnaelLake <bewuketulake1212@gmail.com>
2023-01-02 16:06:20 +0300

    The selection sort of geeks is done.

---
+++ selectionSort.js
@@ -0,0 +1,17 @@
+selectionSort(arr,n){
+    //code here
+    var str = '';
+    for(var i = 1;i<=n;i++){
+        str = '';
+        for(var j=0;j<i;j++){
+            if(arr[i]<arr[j]){
+                var temp = arr[i];
+                arr[i]  = arr[j];
+                arr[j] = temp;
+            }
+            str  = str + arr[j] + ' ';
+        }
+   
+    }
+ 
+   }
