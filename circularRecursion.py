e389488048bc340cf720ab9ef35fe399fd714baf
natnaelLake <bewuketulake1212@gmail.com>
2023-01-14 11:19:23 +0300

    the fibonnaci commit is done

---
+++ circularRecursion.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+    
+        def f(n: int, k: int) -> int:
+            if n == 1:
+                return 0
+            return (f(n - 1, k) + k) % n
+        return f(n, k) + 1
