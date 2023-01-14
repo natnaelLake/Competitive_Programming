aba93741ab6fc5edee1f373b25dd61c3eb921c0b
natnaelLake <bewuketulake1212@gmail.com>
2023-01-14 13:09:01 +0300

    the first linked list commit is done

---
+++ middleLinkedList.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        start = result = head
+        while start and start.next:
+            result = result.next
+            start = start.next.next
+        return result
